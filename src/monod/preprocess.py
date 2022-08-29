217968fe66ac2a61921354927ebdf481dda83b92
gennadygorin <43971338+gennadygorin@users.noreply.github.com>
2022-08-29 15:50:16 -0700

    bumping up code version

--- src/monod/preprocess.py
+++ src/monod/preprocess.py
@@ -14,7 +14,7 @@ import warnings
 import scipy
 from scipy import sparse
 
-code_ver_global = "023"  # bumping up version mid-August
+code_ver_global = "024"  # bumping up version late August
 
 ########################
 ## Debug and error logging
