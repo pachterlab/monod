f9e718c856579d29ddbab67b2dff3c0c36259487
gennadygorin <43971338+gennadygorin@users.noreply.github.com>
2022-11-24 18:06:57 -0800

    Update nn_toolbox.py

--- src/monod/nn_toolbox.py
+++ src/monod/nn_toolbox.py
@@ -1,5 +1,5 @@
 def bursty_none_logL(p,x):
-	"""Compute the Kullback-Leibler divergence between data and a fit.
+	"""Compute the log-likelihood of data microstates.
 
     Parameters
     ----------
@@ -11,10 +11,10 @@ def bursty_none_logL(p,x):
     Returns
     -------
     log_proposal: float np.ndarray
-    	The log-likelihood of each state in Nstates.
+        The log-likelihood of each state in Nstates.
     """
 
-	raise ValueError('Not yet implemented!')
+    raise ValueError('Not yet implemented!')
 
 def bursty_none_grid(p,lm):
     """Evaluate the PMF of the model over a grid at a set of parameters.
@@ -33,4 +33,4 @@ def bursty_none_grid(p,lm):
         the steady-state model PMF over a grid (0,...,limits[0]-1) x (0,...,limits[1]-1).
     """
 
-	raise ValueError('Not yet implemented!')
+    raise ValueError('Not yet implemented!')
