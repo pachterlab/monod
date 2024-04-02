c1766952f12b9682862697a8fc1adfdbb5b7eb35
tarachari3 <tarachari3@gmail.com>
2024-04-02 09:22:07 -0700

    Update preprocess.py

--- src/monod/preprocess.py
+++ src/monod/preprocess.py
@@ -14,7 +14,7 @@ import warnings
 import scipy
 from scipy import sparse
 
-code_ver_global = "026"  # bumping up version early Nov
+code_ver_global = "027"  # bumping up version April 2024
 
 ########################
 ## Debug and error logging
